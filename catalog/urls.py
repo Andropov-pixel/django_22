2087f743186dc06b50abc42a46e0ea643df23cc9
Andropov <line.adsa@bk.ru>
2025-07-10 13:26:48 +0300

    Django_22

---
+++ catalog/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from .views import HomeView, ContactsView, ProductDetailView
+
+app_name = 'catalog'
+
+urlpatterns = [
+    path('home/', HomeView.as_view(), name='home'),
+    path('contacts/', ContactsView.as_view(), name='contacts'),
+    path('products/<int:product_id>/', ProductDetailView.as_view(), name='products'),
+]
---
+++ catalog/views.py
@@ -0,0 +1,21 @@
+from django.shortcuts import render, get_object_or_404
+from django.views.generic import ListView, TemplateView, DetailView
+
+from catalog.models import Product
+
+class HomeView(ListView):
+    model = Product
+    template_name = 'catalog/home.html'
+    context_object_name = 'products'
+
+
+
+class ContactsView(TemplateView):
+    template_name = 'catalog/contacts.html'
+
+
+class ProductDetailView(DetailView):
+    model = Product
+    template_name = 'catalog/products.html'
+    context_object_name = 'product'
+    pk_url_kwarg = 'product_id'  # соответствие параметру из URL
